fa7c1f37d56908a9c642cffa0e3634e08d57b796
Silvia Cortès <scortesg9527@gmail.com>
2022-07-26 15:14:45 -0500

    Genre buttons

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,90 @@
 const baseURL = 'https://api.themoviedb.org/3'
 const key = '?api_key=7f65f07a5ffa52563f8a6b45d629f577'
 const imageURL = 'https://image.tmdb.org/t/p/w500/'
-const mainCarousel = document.querySelector('#carouselUpcoming')
 
+//get variables in the DOM
+const displayGenres = document.getElementById('genresDisplay')
+const mainCarousel = document.querySelector('#carouselUpcoming')
+const tagsElement = document.getElementById('sections')
+const mainContainer = document.getElementById('mainContainer')
+
+//button to show the available genres NOTE: HIDE IS NOT WORKING
+displayGenres.addEventListener('click', () => {    
+    tagsElement.innerHTML = '';
+    genres.forEach(genre =>{
+        const tagEl = document.createElement('button');
+        tagEl.classList.add('col' , 'mb-3' ,'rounded-pill', 'bg-danger', 'm-3', 'p-1', 'text-center' , 'tag');
+        tagEl.id = genre.id;
+        tagEl.innerText = genre.name;
+        tagsElement.append(tagEl)
+    })
+})
+
+//upcoming movies carousel
 const carouselUpcomingFunction = async () => {
 
     try {
         const response = await fetch(`${baseURL}/movie/upcoming${key}`)
-        console.log(response)
-
+//verify the response status
         if (response.status === 200) {
             const data = await response.json();
-            
-
             data.results.forEach(movie => {
                 const carouselMoviesUpcoming = document.createElement('div')
                 carouselMoviesUpcoming.classList.add('carousel-item')
                 carouselMoviesUpcoming.innerHTML = `
                     <img src="${imageURL}${movie.poster_path}" class="d-block w-100" alt="...">
                 `
-
                 mainCarousel.appendChild(carouselMoviesUpcoming);
             });
+        }else if(response.status == 401){
+            clearMainSection();
+            const errorMessage = document.createElement('div')
+            errorMessage.classList.add('container')
+            errorMessage.innerHTML = `
+                <div class="row">
+                    <div class="col-12">
+                        <p class="fs-1 pt-5 text-danger text-center fw-bolder">Error: ${response.status}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p class="fs-1 pt-5 text-white text-center fw-bolder">Something went wrong. API Key invalid</p>
+                    </div>
+                </div>
+            `
+            mainContainer.appendChild(errorMessage)
+        }else if(response.status == 404){
+            clearMainSection();
+            const errorMessage = document.createElement('div')
+            errorMessage.classList.add('container')
+            errorMessage.innerHTML = `
+                <div class="row">
+                    <div class="col-12">
+                        <p class="fs-1 pt-5 text-danger text-center fw-bolder">Error: ${response.status}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p class="fs-1 pt-5 text-white text-center fw-bolder">Something went wrong. Results were not found</p>
+                    </div>
+                </div>
+            `
+            mainContainer.appendChild(errorMessage)
+        }else{
+
         }
 
     } catch (error) {
         console.log(error)
     }
 
-
 }
+
+function clearMainSection(){
+    mainContainer.innerHTML = ''
+}
+
 
 //call functions to execute
 carouselUpcomingFunction();
+displayGenres();
